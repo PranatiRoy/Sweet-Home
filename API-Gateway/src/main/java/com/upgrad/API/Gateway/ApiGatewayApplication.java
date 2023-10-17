f875778ce96397b82e44eb6ca865fa38831d78bb
PranatiRoy <74383850+PranatiRoy@users.noreply.github.com>
2023-10-17 14:45:38 +0530

    Add files via upload

---
+++ API-Gateway/src/main/java/com/upgrad/API/Gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.upgrad.API.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
